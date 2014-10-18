081e9e0c3a28d551364aff6f9b281cd5b6981812
coddo <codobanclaudiu@gmail.com>
2014-10-18 21:51:12 +0300

    Made use of preprocessor directives for easier handling of unit tests

--- CollegeBuffer.DAL/ContextInitializers/DatabaseContextInitializer.cs
+++ CollegeBuffer.DAL/ContextInitializers/DatabaseContextInitializer.cs
@@ -1,8 +1,21 @@
-﻿using System.Data.Entity;
+﻿#define TESTS
+//#undef TESTS
+using System.Data.Entity;
 using CollegeBuffer.DAL.Context;
 
 namespace CollegeBuffer.DAL.ContextInitializers
 {
+#if TESTS
+
+    internal class DatabaseContextInitializer : DropCreateDatabaseAlways<DatabaseContext>
+    {
+        public override void InitializeDatabase(DatabaseContext context)
+        {
+            base.InitializeDatabase(context);
+        }
+    }
+
+#else
     internal class DatabaseContextInitializer : CreateDatabaseIfNotExists<DatabaseContext>
     {
         public override void InitializeDatabase(DatabaseContext context)
@@ -10,4 +23,6 @@ namespace CollegeBuffer.DAL.ContextInitializers
             base.InitializeDatabase(context);
         }
     }
+
+#endif
 }
